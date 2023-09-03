6a807430de9ef57c5fc6271fa739876494a983b6
Prateek Takthar <prateektakthar184b@gmail.com>
2023-09-03 21:43:09 +0530

    update

---
+++ index.js
@@ -0,0 +1,21 @@
+import { log } from "console";
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io"
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+const _filename = fileURLToPath(import.meta.url)
+const _dirname = dirname(_filename)
+
+const app = express();
+const httpServer = createServer(app);
+
+const io = new Server(httpServer);
+
+const port = process.env.PORT || 9090;
+httpServer.listen(port, () => console.log(`App is listening on ${port}`));
+
+app.get('/', (req, res, next) => {
+    res.sendFile(_dirname + "/public/index.html");
+});
+app.use(express.static(_dirname + "/public"))
